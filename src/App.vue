72ffc0acb21fd9ac9daa34a42a572162c6f150d3
muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2021-12-18 23:44:13 +0700

    create category list

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <template>
-  <h1>hello world</h1>
+  <div class="container">
+    <category-list />
+  </div>
 </template>
 <script lang="ts">
-  import { defineComponent } from 'vue'
-export default defineComponent({});
+import { defineComponent } from 'vue'
+import CategoryList from './CategoryList.vue'
+export default defineComponent({
+  components: {
+    CategoryList
+  }
+})
 
 </script>
+<style lang="scss">
+  .container {
+    box-sizing: border-box;
+    overflow-y: scroll;
+    max-width: 320px;
+    margin-left: auto;
+    margin-right: auto;
+    -webkit-box-shadow: 4px 10px 16px -5px rgba(0,0,0,0.75);
+    -moz-box-shadow: 4px 10px 16px -5px rgba(0,0,0,0.75);
+    box-shadow: 4px 10px 16px -5px rgba(0,0,0,0.75);
+  }
+</style>
---
+++ src/CategoryList.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="categories">
+    <p 
+      class="item"
+      v-for="(category, idx) in list"
+      :key="idx"
+      @click="setCategory(category)"
+    >
+      {{ category }}
+    </p>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'CategoryList',
+  data() {
+    return {
+      list: [
+        'general', 
+        'entertainment', 
+        'business', 
+        'health', 
+        'science', 
+        'sports',
+        'technology'
+      ]
+    }
+  },
+  methods: {
+    setCategory(category: string): void {
+      
+    }
+  }
+})
+</script>
+<style lang="scss">
+.categories {
+  background-color: #333;
+  overflow: auto;
+  white-space: nowrap;
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
+  z-index: 6;
+  text-align: center;
+}
+.categories .item {
+  margin-bottom: 0px;
+  cursor: pointer;
+  margin-left: 13px;
+  display: inline-block;
+  color: white;
+  text-align: center;
+  padding: 14px;
+  text-decoration: none;
+}
+</style>
