446af79b97afdc6e9958d348980be0571e94c40d
muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2021-12-18 16:27:32 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>hello world</h1>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent ({
+  name: 'App'
+})
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { VueLoaderPlugin } = require("vue-loader");
+const path = require("path");
+
+module.exports = {
+	output: {
+    publicPath: "http://localhost:8081/",
+  },
+
+  resolve: {
+    extensions: [".vue", ".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 8081,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+
+  plugins: [
+    new VueLoaderPlugin(),
+    new ModuleFederationPlugin({
+      name: "vuehost",
+      filename: "remoteEntry.js",
+      remotes: {},
+      exposes: {},
+      shared: require("./package.json").dependencies,
+    }),
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+}
